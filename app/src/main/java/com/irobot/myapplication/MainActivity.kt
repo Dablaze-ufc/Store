632fdd4120bf74a49e3aba19b447108f67720c62
Dablaze-ufc <chukwuwauchenna@gmail.com>
2020-03-01 23:45:13 +0100

    correct the login button and change it to java 8

--- app/src/main/java/com/irobot/myapplication/MainActivity.kt
+++ app/src/main/java/com/irobot/myapplication/MainActivity.kt
@@ -1,12 +1,7 @@
 package com.irobot.myapplication
 
-import android.app.AlertDialog
-import android.app.Dialog
-import android.content.DialogInterface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
-import com.google.android.material.dialog.MaterialAlertDialogBuilder
 
 class MainActivity : AppCompatActivity() {
 
