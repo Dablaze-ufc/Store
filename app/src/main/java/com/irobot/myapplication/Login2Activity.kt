61d91a296086dfb7db39fb9eb7a9544939a029d7
Dablaze-ufc <chukwuwauchenna@gmail.com>
2020-03-06 02:02:39 +0100

    reDesigned the UI

---
+++ app/src/main/java/com/irobot/myapplication/Login2Activity.kt
@@ -0,0 +1,93 @@
+package com.irobot.myapplication
+
+
+import android.animation.Animator
+import android.animation.AnimatorListenerAdapter
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.view.View.INVISIBLE
+import android.view.View.VISIBLE
+import androidx.core.content.ContextCompat
+import androidx.databinding.DataBindingUtil
+import com.irobot.myapplication.databinding.ActivityLogin2Binding
+import com.irobot.myapplication.ui.auth.RegisterFragment
+import com.irobot.myapplication.ui.auth.SignInFragment
+import com.irobot.myapplication.utils.FlexibleFrameLayout.Companion.ORDER_LOGIN_STATE
+import com.irobot.myapplication.utils.FlexibleFrameLayout.Companion.ORDER_SIGN_UP_STATE
+import com.irobot.myapplication.utils.OnButtonSwitchedListener
+
+class Login2Activity : AppCompatActivity() {
+    private lateinit var binding: ActivityLogin2Binding
+    private var isLogin = true
+    private var listner: OnButtonSwitchedListener? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding =
+            DataBindingUtil.setContentView(this, R.layout.activity_login2)
+
+        val registerFragment = RegisterFragment()
+        val signInFragment =
+            SignInFragment()
+        supportFragmentManager.beginTransaction().replace(R.id.sign_up_fragment, registerFragment)
+            .replace(R.id.login_fragment, signInFragment)
+            .commit()
+        binding.loginFragment.rotation = -90f
+        binding.button.setOnSignUpListener(registerFragment)
+        binding.button.setOnLoginListener(signInFragment)
+        binding.button.setOnClickListener { view ->
+            switchFragment(view)
+        }
+        binding.button.setOnButtonSwitched(object : OnButtonSwitchedListener {
+            override fun onButtonSwitched(isLogin: Boolean) {
+                binding.root.setBackgroundColor(
+                    ContextCompat.getColor(
+                        this@Login2Activity,
+                        if (isLogin) R.color.colorPrimary else R.color.secondPage
+                    )
+                )
+            }
+
+        })
+    }
+
+    override fun onWindowFocusChanged(hasFocus: Boolean) {
+        super.onWindowFocusChanged(hasFocus)
+        binding.loginFragment.pivotX = (binding.loginFragment.width / 2).toFloat();
+        binding.loginFragment.pivotY = binding.loginFragment.height.toFloat();
+        binding.signUpFragment.pivotX = (binding.signUpFragment.width / 2).toFloat();
+        binding.signUpFragment.pivotY = binding.signUpFragment.height.toFloat();
+
+    }
+
+    fun switchFragment(view: View) {
+        if (isLogin) {
+            binding.loginFragment.visibility = VISIBLE
+            binding.loginFragment.animate().rotation(0f).setListener(object :
+                AnimatorListenerAdapter() {
+                override fun onAnimationEnd(animation: Animator?) {
+                    super.onAnimationEnd(animation)
+                    binding.signUpFragment.visibility = INVISIBLE
+                    binding.signUpFragment.rotation = -90f
+                    binding.wrapper.setDrawOrder(ORDER_LOGIN_STATE)
+                }
+
+
+            })
+        } else {
+            binding.signUpFragment.visibility = VISIBLE
+            binding.signUpFragment.animate().rotation(0f)
+                .setListener(object : AnimatorListenerAdapter() {
+                    override fun onAnimationEnd(animation: Animator?) {
+                        super.onAnimationEnd(animation)
+                        binding.loginFragment.visibility = INVISIBLE
+                        binding.loginFragment.rotation = -90f
+                        binding.wrapper.setDrawOrder(ORDER_SIGN_UP_STATE)
+                    }
+                })
+        }
+        isLogin = !isLogin
+        binding.button.startAnimation()
+    }
+}
---
+++ app/src/main/java/com/irobot/myapplication/ui/auth/RegisterFragment.java
@@ -0,0 +1,124 @@
+package com.irobot.myapplication.ui.auth;
+
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.button.MaterialButton;
+import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthUserCollisionException;
+import com.google.firebase.auth.FirebaseUser;
+import com.irobot.myapplication.R;
+import com.irobot.myapplication.utils.OnSignUpListener;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class RegisterFragment extends Fragment implements OnSignUpListener {
+
+    String registerEmail, registerPassword, registerConfirmPassword;
+    private TextInputLayout
+            mInputLayoutRegisterEmail,
+            mInputLayoutRegisterPassword,
+            mInputLayoutRegisterConfirmPassword;
+    private FirebaseAuth mFirebaseRegister;
+    private MaterialButton mMaterialButtonRegister;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_register, container, false);
+
+        //find View by Id's
+        mInputLayoutRegisterEmail = view.findViewById(R.id.textInputLayout_register_email);
+        mInputLayoutRegisterPassword = view.findViewById(R.id.textInputLayout_register_password);
+        mInputLayoutRegisterConfirmPassword = view.findViewById(R.id.textInputLayout_register_confirm_password);
+//        mMaterialButtonRegister = view.findViewById(R.id.button_register);
+
+        //get Instance
+        mFirebaseRegister = FirebaseAuth.getInstance();
+
+        //method Calling
+
+
+        return view;
+    }
+
+    private void registerWithStore() {
+        registerEmail = mInputLayoutRegisterEmail.getEditText().getText().toString().trim();
+        registerPassword = mInputLayoutRegisterPassword.getEditText().getText().toString().trim();
+        registerConfirmPassword = mInputLayoutRegisterConfirmPassword.getEditText().getText().toString().trim();
+
+        if (checkingPasswords()) {
+            mFirebaseRegister.createUserWithEmailAndPassword(registerEmail, registerPassword).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if (task.isSuccessful()) {
+                        Toast.makeText(getContext(), "Registration Completed", Toast.LENGTH_SHORT).show();
+//                        signOut();
+                        //signIn successful
+                        FirebaseUser user = mFirebaseRegister.getCurrentUser();
+                        user.sendEmailVerification().addOnCompleteListener(new OnCompleteListener<Void>() {
+                            @Override
+                            public void onComplete(@NonNull Task<Void> task) {
+                                if (task.isSuccessful()) {
+                                    Toast.makeText(getContext(), "Check your Email for verification", Toast.LENGTH_SHORT).show();
+                                    navigateToSignIn(mMaterialButtonRegister);
+                                    signOut();
+                                } else
+                                    Toast.makeText(getContext(), "Could'nt send verification! " + task.getException().getLocalizedMessage(), Toast.LENGTH_SHORT).show();
+                            }
+                        });
+                    } else {
+                        if (task.getException() instanceof FirebaseAuthUserCollisionException) {
+                            Toast.makeText(getContext(), "You are already Registered ", Toast.LENGTH_SHORT).show();
+                        } else
+                            Toast.makeText(getContext(), "an Error occured", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+        } else {
+            Toast.makeText(getContext(), "Password does'nt match", Toast.LENGTH_SHORT).show();
+        }
+
+
+    }
+
+//    private void whenButtonIsClicked() {
+//        mMaterialButtonRegister.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                registerWithStore();
+//            }
+//        });
+//    }
+
+    private Boolean checkingPasswords() {
+        return registerPassword.length() == registerConfirmPassword.length();
+    }
+
+    private void navigateToSignIn(View view) {
+
+    }
+
+    private void signOut() {
+        if (mFirebaseRegister.getCurrentUser() != null) {
+            mFirebaseRegister.signOut();
+        }
+    }
+
+    @Override
+    public void signUp() {
+
+    }
+}
